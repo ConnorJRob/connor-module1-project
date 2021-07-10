13482f0fbc985db9470db659143413c88a1abb5a
Connor <connorjrobertson@hotmail.co.uk>
2021-07-10 15:00:03 +0100

    First iteration of Adventurer class created in Adventurer.py

---
+++ models/adventurer.py
@@ -0,0 +1,7 @@
+class Adventurer:
+    def __init__(self, first_name, last_name, adventurer_class = None, id = None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.adventurer_class = adventurer_class
+        self.id = id
+
