44de839d77516964baaebd1f8644cde134603b98
Connor <connorjrobertson@hotmail.co.uk>
2021-07-10 15:23:03 +0100

    Console.py file set up with import lines

---
+++ console.py
@@ -0,0 +1,8 @@
+import pdb
+from models.adventurer import Adventurer
+from models.lesson import Lesson
+from models.enrolment import Enrolment
+
+import repositories.adventurer_repository as adventurer_repository
+import repositories.lesson_repository as lesson_repository
+import repositories.enrolment_repository as enrolment_repository
