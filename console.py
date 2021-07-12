8873ea40b4e2f42fb628a248f9ac83cd16832679
Connor <connorjrobertson@hotmail.co.uk>
2021-07-12 11:07:18 +0100

    Wrote enrolment save function and enrolment select_all() function and used console to check they are working as expected

--- console.py
+++ console.py
@@ -9,6 +9,7 @@ import repositories.enrolment_repository as enrolment_repository
 
 
 # adventurer_repository.delete_all()
+# lesson_repository.delete_all()
 
 # adventurer_1 = Adventurer("Zimazz", "Greenwind", "Wizard")
 # adventurer_repository.save(adventurer_1)
@@ -19,38 +20,23 @@ import repositories.enrolment_repository as enrolment_repository
 # adventurer_3 = Adventurer("Grog", "Strongjaw", "Barbarian")
 # adventurer_repository.save(adventurer_3)
 
-# adventurers = adventurer_repository.select_all()
-
-# adventurer_to_update = adventurer_repository.select(1)
-
-# adventurer_to_update.adventurer_class = "Sorcerer"
-
-# adventurer_repository.delete_adventurer(1)
-
-# adventurer_repository.update_adventurer(adventurer_to_update)
-
-# adventurers = adventurer_repository.select_all()
-
 # lesson_1 = Lesson("Orcish for Beginners", "Wizards", "A beginner's class in speaking Orcish, making sure to avoid getting killed for saying something about someone's mother by accident")
-
 # lesson_repository.save(lesson_1)
 
 # lesson_2 = Lesson("How and when to use Fireball", "Wizards", "Not all the time.")
-
 # lesson_repository.save(lesson_2)
 
 # lesson_3 = Lesson("When is it a mimic?", "Bards", "Assume always.")
-
 # lesson_repository.save(lesson_3)
 
-lesson_repository.delete_lesson(8)
+# enrolment_1 = Enrolment(adventurer_1, lesson_1)
+# enrolment_2 = Enrolment(adventurer_2, lesson_2)
+# enrolment_3 = Enrolment(adventurer_3, lesson_3)
 
-# lesson_1.lesson_description = "SPEAKING ORC, THIS IS A TEST"
+# enrolment_repository.save(enrolment_1)
+# enrolment_repository.save(enrolment_2)
+# enrolment_repository.save(enrolment_3)
 
-# lesson_repository.update_lesson(lesson_1)
-
-lessons = lesson_repository.select_all()
-
-# lessons = lesson_repository.select_all()
+enrolment_list = enrolment_repository.select_all()
 
 pdb.set_trace()
--- repositories/enrolment_repository.py
+++ repositories/enrolment_repository.py
@@ -3,15 +3,35 @@ from models.adventurer import Adventurer
 from models.lesson import Lesson
 from models.enrolment import Enrolment
 
+import repositories.adventurer_repository as adventurer_repository
+import repositories.lesson_repository as lesson_repository
+
 ##CREATE
 ##save enrolment
+def save(enrolment):
+    sql = "INSERT INTO enrolments (adventurer_id, lesson_id) VALUES (%s, %s) RETURNING id"
+    values = [enrolment.adventurer.id, enrolment.lesson.id]
+    results = run_sql(sql, values)
+    enrolment.id = results[0]['id']
+    return enrolment
 
 ##READ
 ##select all enrolments
-##select enrolment(id)
+def select_all():
+    enrolments = []
 
-#UPDATE
-##update enrolment
+    sql = "SELECT * FROM enrolments"
+    results = run_sql(sql)
+
+    for row in results:
+        adventurer = adventurer_repository.select(row['adventurer_id'])
+        lesson = lesson_repository.select(row['lesson_id'])
+        enrolment = Enrolment(adventurer, lesson, row['id'])
+        enrolments.append(enrolment)
+    return enrolments
+
+##return the lesson associated with a specific enrolment
+##return the adventurer associated with a specific enrolment
 
 #DELETE
 ##delete all
