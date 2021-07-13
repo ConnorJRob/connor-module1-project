f2e80e80b427b6b66ce93b2949a790fc573d4999
Connor <connorjrobertson@hotmail.co.uk>
2021-07-13 10:49:20 +0100

    Adventurers index template content established

--- controllers/adventurer_controller.py
+++ controllers/adventurer_controller.py
@@ -4,3 +4,8 @@ from models.adventurer import Adventurer
 import repositories.adventurer_repository as adventurer_repository
 
 adventurers_blueprint = Blueprint("adventurers", __name__)
+
+@adventurers_blueprint.route("/adventurers")
+def adventurers():
+    adventurers = adventurer_repository.select_all()
+    return render_template("adventurers/index.html", adventurers=adventurers)
