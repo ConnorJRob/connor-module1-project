40f692c8392f68f19197daac77d658dc74b37540
Connor <connorjrobertson@hotmail.co.uk>
2021-07-13 10:31:13 +0100

    Route to lessons url is working

--- controllers/lesson_controller.py
+++ controllers/lesson_controller.py
@@ -4,3 +4,8 @@ from models.lesson import Lesson
 import repositories.lesson_repository as lesson_repository
 
 lessons_blueprint = Blueprint("lessons", __name__)
+
+@lessons_blueprint.route("/lessons")
+def lessons():
+    lessons = lesson_repository.select_all()
+    return render_template("lessons/index.html", lessons=lessons)
