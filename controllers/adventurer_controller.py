f592f06ff859c0754980fc2777e9e0f9df8cffa4
Connor <connorjrobertson@hotmail.co.uk>
2021-07-13 15:33:27 +0100

    Edit Adventurer template & route established and tested

--- controllers/adventurer_controller.py
+++ controllers/adventurer_controller.py
@@ -5,7 +5,53 @@ import repositories.adventurer_repository as adventurer_repository
 
 adventurers_blueprint = Blueprint("adventurers", __name__)
 
+# SHOW ALL
+# GET '/adventurers
 @adventurers_blueprint.route("/adventurers")
 def adventurers():
     adventurers = adventurer_repository.select_all()
     return render_template("adventurers/index.html", adventurers=adventurers)
+
+# SHOW 
+# GET '/adventurers/id
+@adventurers_blueprint.route("/adventurers/<id>")
+def show(id):
+    adventurer = adventurer_repository.select(id)
+    lessons = adventurer_repository.lessons(adventurer)
+    return render_template("adventurers/show.html", adventurer=adventurer, lessons=lessons)
+
+# NEW
+# GET '/lessons/new
+@adventurers_blueprint.route("/adventurers/new")
+def new_adventurer():
+    return render_template("adventurers/new.html")
+
+# CREATE
+# POST '/adventurers'
+@adventurers_blueprint.route("/adventurers", methods=['POST'])
+def create_adventurer():
+    first_name = request.form['adventurer-first-name']
+    last_name = request.form['adventurer-last-name']
+    adventurer_class = request.form['adventurer-class']
+    adventurer = Adventurer(first_name, last_name, adventurer_class)
+    adventurer_repository.save(adventurer)
+    return redirect("/adventurers")
+
+# EDIT
+# GET '/adventurers/<id>/edit'
+@adventurers_blueprint.route("/adventurers/<id>/edit")
+def edit_adventurer(id):
+    adventurer = adventurer_repository.select(id)
+    adventurer_classes = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
+    return render_template("adventurers/edit.html", adventurer=adventurer, adventurer_classes=adventurer_classes)
+
+# UPDATE
+# PUT '/adventurers/<id>'
+@adventurers_blueprint.route("/adventurers/<id>", methods=['POST'])
+def update_adventurer(id):
+    adventurer = adventurer_repository.select(id)
+    adventurer.first_name = request.form['adventurer-first-name']
+    adventurer.last_name = request.form['adventurer-last-name']
+    adventurer.adventurer_class = request.form['adventurer-class']
+    adventurer_repository.update_adventurer(adventurer)
+    return redirect("/adventurers")
--- controllers/lesson_controller.py
+++ controllers/lesson_controller.py
@@ -5,7 +5,32 @@ import repositories.lesson_repository as lesson_repository
 
 lessons_blueprint = Blueprint("lessons", __name__)
 
+# GET
 @lessons_blueprint.route("/lessons")
 def lessons():
     lessons = lesson_repository.select_all()
     return render_template("lessons/index.html", lessons=lessons)
+
+# GET /lessons/id
+@lessons_blueprint.route("/lessons/<id>")
+def show(id):
+    lesson = lesson_repository.select(id)
+    adventurers = lesson_repository.adventurers(lesson)
+    return render_template("lessons/show.html", lesson=lesson, adventurers=adventurers)
+
+# NEW
+# GET '/lessons/new
+@lessons_blueprint.route("/lessons/new")
+def new_lesson():
+    return render_template("lessons/new.html")
+
+# CREATE
+# POST '/lessons'
+@lessons_blueprint.route("/lessons", methods=['POST'])
+def create_lesson():
+    lesson_name = request.form['lesson-name']
+    recommended_for = request.form['recommended_for']
+    lesson_description = request.form['lesson_description']
+    lesson = Lesson(lesson_name, recommended_for, lesson_description)
+    lesson_repository.save(lesson)
+    return redirect("/lessons")
